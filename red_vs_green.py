5d3ddf408ffbca3a17cbe67b7dfa32835ac842b1
kiril_km <kirov.ppv@gmail.com>
2020-07-25 11:53:11 +0300

    Remove input prompts to resemble the samples

--- red_vs_green.py
+++ red_vs_green.py
@@ -16,12 +16,13 @@ def is_grid_valid(s):
         print("Value Error either in X or Y")
         return False
     if a <= b < 1000:
-        print('check passed successfully')
         return True
     print('False')
     return False
+
+
 def input_grid_size():
-    grid_size = input('Type size of grid in the format x,y:')
+    grid_size = input()
     while not is_grid_valid(grid_size):
         print('in the while loop')
         grid_size = input('Type x,y (size of grid):')
@@ -33,6 +34,7 @@ def input_grid_size():
     r = int(r_c_lst[1])
     return r, c
 
+
 # input initial values of the cells in the grid
 def is_row_valid(row_str, n):
     pattern = r"\b([0-1]{%d})\b" % n
@@ -40,16 +42,19 @@ def is_row_valid(row_str, n):
     if match is None:
         return False
     return True
+
+
 def input_grid(size):
     """creates the initial grid and setts the values of each cell from the user's input"""
     grid_fill = list()
     cols, rows = size[1], size[0]
     for i in range(rows):
-        string_input_row = input(f'row {i} --> ')
+        string_input_row = input()
         while not is_row_valid(string_input_row, cols):
-            string_input_row = input(f'row {i} --> ')
+            string_input_row = input()
         grid_fill.append([int(x) for x in string_input_row])
     return grid_fill
+
 
 # input of coordinates of the cell in question and total number of generations to be made
 def is_coord_num_valid(coord_num_str, x_grid, y_grid):
@@ -63,29 +68,30 @@ def is_coord_num_valid(coord_num_str, x_grid, y_grid):
             0 <= y_cell <= y_grid - 1:
         return True
     return False
+
+
 def input_coord_cell_gen(size):
     """takes the user input for coordinates of a cell and number of generation"""
-    coord_num = input('Coordinates of a cell and a number of generation x,y,gen -->')
+    coord_num = input()
     cols, rows = size[1], size[0]
     while not is_coord_num_valid(coord_num, cols, rows):
-        coord_num = input('Enter in format x1,y1,num_of_gen -->')
+        coord_num = input()
     x_c, y_c, n_gen = [int(x) for x in coord_num.split(',')]
     return x_c, y_c, n_gen
+
+
 def counts_cell_is_green(gen_obj, input_coordCell_genNum):
-    gen_num= input_coordCell_genNum[2]
-    r=input_coordCell_genNum[1]
-    c=input_coordCell_genNum[0]
+    gen_num = input_coordCell_genNum[2]
+    r = input_coordCell_genNum[1]
+    c = input_coordCell_genNum[0]
 
     times_cell_is_green = 0
     if gen_obj.grid[r][c].is_cell_green():
         times_cell_is_green += 1
-    print(f'Generation 0\ncell @ row:{r} col:{c} -- > times green:{times_cell_is_green}')
     for gen in range(1, gen_num + 1):
         gen_obj.create_next_generation()
         if gen_obj.grid[r][c].is_cell_green():
             times_cell_is_green += 1
-        print(f'Generation {gen}\ncell @ row:{r} col:{c} -- > times green:{times_cell_is_green}')
-        gen_obj.grid_vizualize()
     return times_cell_is_green
 
 
@@ -94,7 +100,7 @@ class Generation:
     def __init__(self, matrix):
         matrix_red_green = list()
         for i in range(len(matrix)):
-            temp_row=list()
+            temp_row = list()
             for k in range(len(matrix[0])):
                 if matrix[i][k] == 1:
                     temp_row.append(Green(i, k, 0))
@@ -104,19 +110,18 @@ class Generation:
         self.grid = matrix_red_green
 
     def count_green_neighbors(self, row_center_cell, col_center_cell):
-        # point the cell in question about its neighbors
-        cell_center = self.grid[row_center_cell][col_center_cell]
+        # counts the green cells around the cell in question
         rows = len(self.grid)
         cols = len(self.grid[0])
         greens_count = 0
-        # the loop roams one cell away to check neighboring cells
-        # and traverses this domain row by row left to right
-        for i in range(3):  # for row in [row_center_cell-1, row_center_cell, row_center_cell+1]
-            roam_cell_row = row_center_cell + i - 1
+        # the loop roams one cell away around the center cell to
+        # traverses this domain of neighboring cells
+        for row in [row_center_cell - 1, row_center_cell, row_center_cell + 1]:
+            roam_cell_row = row
             if roam_cell_row < 0 or roam_cell_row > rows - 1:
                 continue
-            for k in range(3):
-                roam_cell_col = col_center_cell + k - 1
+            for col in [col_center_cell - 1, col_center_cell, col_center_cell + 1]:
+                roam_cell_col = col
                 if roam_cell_col < 0 or roam_cell_col > cols - 1:
                     continue
                 if roam_cell_col == col_center_cell and roam_cell_row == row_center_cell:
@@ -127,8 +132,9 @@ class Generation:
                 if roam_cell.is_cell_green():
                     greens_count += 1
         return greens_count
+
     def create_next_generation(self):
-        nextmatrix = list()  # is it possible rigid size --- DONE it using list of list of Nones
+        nextmatrix = list()
         rows = len(self.grid)
         for i in range(rows):
             temp_cols = list()
@@ -142,52 +148,49 @@ class Generation:
             for col in range(cols):
                 greens_count = self.count_green_neighbors(row_center_cell=row, col_center_cell=col)
                 cell = self.grid[row][col]
-                #if cell_val == 1 and greens_count in [0, 1, 4, 5, 7, 8]:  # change in next generation     1 -- > 0
-                #    nextmatrix[row][col] = 0
-                #elif cell_val == 0 and greens_count in [3, 6]:  # change in next generation     0 -- > 1
-                #    nextmatrix[row][col] = 1
-                #else:
-                #    nextmatrix[row][col] = cell_val  # stay unchanged in next generation 0 -- > 0 & 1 -- > 1
                 nextmatrix[row][col] = cell.state_in_next_gen(greens_count)
         self.grid = nextmatrix
+
     def grid_vizualize(self):
         """vizualizes grid with its values /for testing purposes/"""
         for i in range(len(self.grid)):
             for k in range(len(self.grid[i])):
                 print(f'{self.grid[i][k].color:>7}', end=' ')
             print()
-class Cell:
-        def __init__(self,r,c,cur_gen):
-            self.row = r
-            self.col = c
-            self.gen = cur_gen
 
-        def is_cell_green(self):
-            return isinstance(self, Green)
+
+class Cell:
+    def __init__(self, r, c, cur_gen):
+        self.row = r
+        self.col = c
+        self.gen = cur_gen
+
+    def is_cell_green(self):
+        return isinstance(self, Green)
+
+
 class Green(Cell):
     value = 1
     color = 'green'
 
-    def state_in_next_gen(self,green_neig_count):
-        if green_neig_count in [0, 1, 4, 5, 7, 8]:              # change in next generation     1 -- > 0
-            return Red(self.row, self.col, self.gen+1 )
-        return Green(self.row, self.col, self.gen+1)  # stay same in next generation  1 -- > 1
+    def state_in_next_gen(self, green_neig_count):
+        if green_neig_count in [0, 1, 4, 5, 7, 8]:  # change in next generation     1 -- > 0
+            return Red(self.row, self.col, self.gen + 1)
+        return Green(self.row, self.col, self.gen + 1)  # stay same in next generation  1 -- > 1
+
+
 class Red(Cell):
     value = 0
     color = 'red'
 
-    def state_in_next_gen(self,green_neig_count):
-        if green_neig_count in [3, 6]:                          # change in next generation     0 -- > 1
-            return Green(self.row, self.col, self.gen+1)
-        return Red(self.row, self.col, self.gen+1)    # stay same in next generation  0 -- > 0
+    def state_in_next_gen(self, green_neig_count):
+        if green_neig_count in [3, 6]:  # change in next generation     0 -- > 1
+            return Green(self.row, self.col, self.gen + 1)
+        return Red(self.row, self.col, self.gen + 1)  # stay same in next generation  0 -- > 0
 
 
 size = input_grid_size()
 gen_zero = Generation(input_grid(size))
-gen_zero.grid_vizualize()
 coorCell_genNum = input_coord_cell_gen(size)
-
-
-print('START the GAME')
 times_green = counts_cell_is_green(gen_zero, coorCell_genNum)
 print(f'Result: {times_green}')
